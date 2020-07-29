20c904926370b3dc04d69b0b3de69404d98c6aca
Nicholas Clark <nicholas@californiaclarks.com>
2020-07-29 15:40:14 -0700

    Side player's list done

--- README.md
+++ README.md
@@ -26,14 +26,14 @@ node server.js
 
 
 #### public/maze.js
->###### Maze class
->>The Maze class is used to create and manage each tile associated with the maze all of the characters roam through.
->###### Wall Class
->>The Wall class is used to denote the position of a wall in the maze. The Maze class contains a list of Wall objects that is only rendered once to improve efficiency.
->###### Food Class
->>The Food class is used to denote the position of food particles in the maze.
->###### Empty Class
->>The Empty class is used to denote the position of empty tiles in the maze. The list of Empty objects in maze is used to prevent trails from following characters.
+###### Maze class
+The Maze class is used to create and manage each tile associated with the maze all of the characters roam through.
+###### Wall Class
+The Wall class is used to denote the position of a wall in the maze. The Maze class contains a list of Wall objects that is only rendered once to improve efficiency.
+###### Food Class
+The Food class is used to denote the position of food particles in the maze.
+###### Empty Class
+The Empty class is used to denote the position of empty tiles in the maze. The list of Empty objects in maze is used to prevent trails from following characters.
 
 ## Known Bugs
 - Because characters only emit UpdatePosition if they are actively moving, new connected users won't be able to see already connected users that are stationary until the already connected user moves (As of commit 39a4c0c)
--- server.js
+++ server.js
@@ -1,17 +1,5 @@
 const debug = true;
-
-/*
-var express = require('express');
-var app = express();
-var serv = require('http').Server(app);
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/client/index.html');
-});
-app.use('/client',express.static(__dirname + '/client'));
-
-serv.listen(2000);
-*/
+const minPlayersPerGame = 2;
 
 //using "EXPRESS" to connect the server to the web page
 var express = require('express');
@@ -22,6 +10,7 @@ var rooms = {};
 
 
 app.use(express.static(__dirname + '/public'));
+
 app.get('/game/*', function(req, res) {
     res.sendFile(__dirname + '/public/game.html');
 });
@@ -50,15 +39,18 @@ io.sockets.on('connection', function (socket) {
 
         room.addUser({
             id: socket.id,
+            name: data.name,
             score: 0
         });
         debugLog(`Room: ${room.id} (${room.numUsers()} Active Users)`);
+        io.sockets.in(data.gameId).emit('UpdateSpectators', {users: room.users});
 
-        //2 IS FOR TESTING ONLY, 4 FOR PRODUCTION
-        if (room.numUsers() >= 2) {
+        if (room.numUsers() >= minPlayersPerGame) {
             //Give the users a chance to update their otherUsers arrays
             setTimeout(() => {
-                io.sockets.in(data.gameId).emit('StartGame', {assignments: room.getCharacterAssignments()});
+                let assignments = room.getCharacterAssignments();
+                io.sockets.in(data.gameId).emit('StartGame', {assignments: assignments});
+                io.sockets.in(data.gameId).emit('UpdateSpectators', {users: room.users, assignments: assignments});
             }, 1000);
         }
 
@@ -70,7 +62,7 @@ io.sockets.on('connection', function (socket) {
         let room = rooms[data.gameId];
         //Increase score of winner and pacman
         if(data.winner != null) {
-            room.users[data.winner].score += 10;
+            room.users[data.winner].score += 20;
         }
         room.users[data.id].score += data.foodEaten;
 
@@ -81,7 +73,9 @@ io.sockets.on('connection', function (socket) {
 
         //Give the users a chance to update their otherUsers arrays
         setTimeout(() => {
-            io.sockets.in(data.gameId).emit('StartGame', {assignments: room.getCharacterAssignments()});
+            let assignments = room.getCharacterAssignments();
+            io.sockets.in(data.gameId).emit('StartGame', {assignments: assignments});
+            io.sockets.in(data.gameId).emit('UpdateSpectators', {users: room.users, assignments: assignments});
         }, 1000);
     });
 
@@ -108,11 +102,15 @@ io.sockets.on('connection', function (socket) {
                 socket.to(roomName).emit('DeleteCharacter', {id: socket.id});
 
                 let room = rooms[roomName];
-                room.deleteUser(socket.id);
+                if (room) {
+                    room.deleteUser(socket.id);
 
-                if (room.numUsers() == 0) {
-                    delete rooms[roomName];
-                    debugLog(`Deleting Room ${roomName} because there are no users`);
+                    if (room.numUsers() == 0) {
+                        delete rooms[roomName];
+                        debugLog(`Deleting Room ${roomName} because there are no users`);
+                    } else if (room.numUsers() < minPlayersPerGame) {
+                        io.sockets.in(roomName).emit('ResetGame');
+                    }
                 }
             }
         }
@@ -170,7 +168,6 @@ class Room {
             i++;
         }
 
-        debugLog(assignments);
         return assignments;
     }
 
